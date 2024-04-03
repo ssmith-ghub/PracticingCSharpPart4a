874501aa6401758c5f170edd1809b97e0ef39feb
Stephen Smith <ssmith_138823@outlook.com>
2024-04-02 20:54:17 -0400

    Pushing to GitHub

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double gotchaDayOne = 5.5; //const prevents this "double" variable from being changed later on
+            double gotchaDayTwo = 4.15;
+            string dogNameOne = "Boomer";
+            string dogNameTwo = "Ace";
+            bool petLove = true;
+            
+
+            Console.WriteLine("We've had " + dogNameOne + " and " + dogNameTwo + " for a combined " + (gotchaDayOne + gotchaDayTwo) + " years!");
+            Console.ReadKey();
+            Console.Write("Do we love the dogss\b?\n\t" + petLove);
+        }
+    }
+}
